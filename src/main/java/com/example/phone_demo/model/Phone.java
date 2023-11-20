3eac2623cbd2befeb22cc07cd410f6c04928035b
Nurbek <nmussabayev@klika-tech.com>
2023-11-20 14:23:19 +0400

    update jdk21

--- src/main/java/com/example/phone_demo/model/Phone.java
+++ src/main/java/com/example/phone_demo/model/Phone.java
@@ -1,9 +1,9 @@
 package com.example.phone_demo.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.util.Date;
 
 
